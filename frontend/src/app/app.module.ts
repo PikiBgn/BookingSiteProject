9d6c0944b348bfe0497ddef74b8b579162a17c45
mike <mikolajpaziewskialfa@gmail.com>
2023-06-23 20:57:12 +0200

    Change of the project structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AboutComponent } from './component/about/about.component';
 import {AppRoutingModule} from "./general/routes/app-routing.module";
-import { BookComponent } from './book/book.component';
-import { AuthComponent } from './auth/auth.component';
-import { RegisterPanelComponent } from './register-panel/register-panel.component';
-import { HomeComponent } from './home/home.component';
+import { BookComponent } from './component/book/book.component';
+import { AuthComponent } from './component/auth/auth.component';
+import { RegisterPanelComponent } from './component/register-panel/register-panel.component';
+import { HomeComponent } from './component/home/home.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AccommodationObjectService} from "./service/accommodation-object/accommodation-object.service";
 import {HttpClientModule} from "@angular/common/http";
 import {TestComponent} from "./component/test/test.component";
+import {AccommodationObjectTypeService} from "./service/accommodation-object-type/accommodation-object-type.service";
+import {LocalizationService} from "./service/localization/localization.service";
+import {ReservationService} from "./service/reservation/reservation.service";
+import {RoomService} from "./service/room/room.service";
+import {UserService} from "./service/user/user.service";
 
 @NgModule({
   declarations: [
@@ -32,7 +37,7 @@ import {TestComponent} from "./component/test/test.component";
     HttpClientModule,
     FormsModule,
   ],
-  providers: [AccommodationObjectService],
+  providers: [AccommodationObjectService, AccommodationObjectTypeService, LocalizationService, ReservationService, RoomService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
